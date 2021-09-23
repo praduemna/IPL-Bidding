740a352ad47637f6710795a708f20d070943019a
Siiddhii <schavan559@gmail.com>
2021-09-23 11:54:11 +0530

    developing Register page UI

--- angular/ipl-bidding-app/src/app/app-routing.module.ts
+++ angular/ipl-bidding-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterPlayerComponent } from './register-player/register-player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'register-player', component: RegisterPlayerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/ipl-bidding-app/src/app/app.module.ts
+++ angular/ipl-bidding-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterPlayerComponent } from './register-player/register-player.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPlayerComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
